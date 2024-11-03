3040bfdde45385e1753bc1044207d59f5d097041
IISCE <iscedigitalconcept@gmail.com>
2024-11-03 07:47:38 +0100

    fix(ui): :bug: remove background from hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,8 +2,8 @@
 
 export default function HeroSection() {
 	return (
-		<div className="relative flex min-h-screen flex-col">
-			<video
+		<div className="relative flex min-h-screen flex-col bg-black">
+			{/* <video
 				autoPlay
 				loop
 				muted
@@ -11,7 +11,7 @@ export default function HeroSection() {
 				className={`absolute inset-0 h-full w-full object-cover transition-opacity duration-1000`}
 			>
 				<source src="/videos/main-video.webm" type="video/webm" />
-			</video>
+			</video> */}
 			<div className="absolute grid h-screen w-full place-items-end bg-gradient-to-b from-black/50 to-black pb-16 md:pb-32">
 				<div className="container mx-auto px-4 text-left text-white">
 					<h1 className="mb-6 text-start text-3xl font-bold md:text-4xl lg:text-5xl">
